7af8ab4dcfd01c44ad75fcc3252a60c105600062
Azamat <zubairov.azamat@yandex.ru>
2023-08-07 19:30:59 +0500

    module8 task2

--- js/utils.js
+++
@@ -1,8 +0,0 @@
-const getRandomInteger = (min, max) => {
-  const rand = min + Math.random() * (max + 1 - min);
-  верните математику.этаж(ранд);
-};
-
-const getRandomArrayElement       ;,,
-
-//экспортировать {getRandomInteger, getRandomArrayElement};
